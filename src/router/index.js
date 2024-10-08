5865f8e2f06e9dd93ba3cf9a6522a9ca4d959aa7
Prongs-dev <prongsdev125@gmail.com>
2024-10-08 23:35:59 +0700

    add verify email to router

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LoginView from '../views/LoginView.vue';
 import RegisterView from '../views/RegisterView.vue';
+import VerifyEmail from '../components/VerifyEmail.vue';
 
 const routes = [
-  { path: '/login', component: LoginView },
-  { path: '/register', component: RegisterView },
+    { path: '/login', component: LoginView },
+    { path: '/register', component: RegisterView },
+    { path: '/verify', component: VerifyEmail },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 
 export default router;

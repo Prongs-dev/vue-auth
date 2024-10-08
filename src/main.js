8dbd7019d77ddfaed043576564b32f89b7daff3e
Prongs-dev <prongsdev125@gmail.com>
2024-10-08 23:36:13 +0700

    update with vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import router from './router';
-import './assets/tailwind.css'; // Import Tailwind CSS
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles'; // Ensure you import Vuetify styles
+import '@mdi/font/css/materialdesignicons.css'; // Optional: Material Design Icons
 
-createApp(App)
-    .use(router)
-    .mount('#app')
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+// Create a Vuetify instance
+const vuetify = createVuetify(
+    components,
+    directives,
+);
+
+const app = createApp(App);
+
+// Use the router
+app.use(router);
+
+// Use Vuetify
+app.use(vuetify);
+
+// Mount the app
+app.mount('#app');
